679a7711b042a24c29fd97247e935806e1d28984
李波 <1043973279@qq.com>
2017-02-07 15:41:47 +0800

    创建主体类

---
+++ utupdater_lib/src/main/java/cn/utsoft/cd/utupdater/UTLoadManager.java
@@ -0,0 +1,24 @@
+package cn.utsoft.cd.utupdater;
+
+/**
+ * Created by 李波 on 2017/2/7.
+ * Function: 主体类: 1. 用于检测是否需要更新
+ * 2. 用于下载apk，
+ * 3. 用于安装apk
+ * 4...
+ * Desc:
+ */
+public class UTLoadManager {
+
+    public static void check() {
+
+    }
+
+    public static void load() {
+
+    }
+
+    public static void install() {
+
+    }
+}
